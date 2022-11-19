e0a0f20832cc90cd7665ce40edf864913e53625e
Spencer Waguespack <wswaguespack403@gmail.com>
2022-11-19 16:06:49 -0500

    addded challenge statment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// create a program to convert temperature units
+// create a program to calc a fibonacci number
+// create a program to print the 12 days of chrismas
+
 fn main() {
     println!("Hello, world!");
 }
